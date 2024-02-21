eacac744f1691aa2a58ad1e9f61531fb7420278d
TobiasClausen <tobias.clausen06@gmail.com>
2024-02-21 12:45:41 +0100

    Spieler ist drin

---
+++ assets/Baustein.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Baustein" tilewidth="17" tileheight="17" tilecount="806" columns="31">
+ <image source="Vorlage.png" width="528" height="448"/>
+</tileset>
---
+++ assets/Map.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Map" tilewidth="16" tileheight="16" tilecount="812" columns="29">
+ <image source="Vorlage.png" width="464" height="448"/>
+</tileset>
